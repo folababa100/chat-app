56ee0041a284e531a7c394065cb5e5c102c33387
Adekunbi Adefolarin <dekunbifola100@gmail.com>
2018-04-25 14:16:19 +0100

    scrolling

--- imports/ui/SearchBar.js
+++ imports/ui/SearchBar.js
@@ -16,7 +16,6 @@ export class SearchBar extends React.Component {
     return (
       <div>
         <form className="boxed-view__form1">
-            <Ionicon className="icon__search" icon="ios-search" fontSize="35px" color="#C9C9C9"/>
             <input type="search" placeholder="Search for chat"/>
         </form>
       </div>
--- imports/ui/SideBar.js
+++ imports/ui/SideBar.js
@@ -5,12 +5,10 @@ import SideBarHeader from "./SideBarHeader";
 
 export default SideBar = () => {
   return (
-    <div>
-      <div className="sidebar__content">
-        <SideBarHeader/>
-        <SearchBar />
-        <ChatSidebar />
-      </div>
+    <div className="sidebar__content">
+      <SideBarHeader />
+      <SearchBar />
+      <ChatSidebar />
     </div>
   )
 }
